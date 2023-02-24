80e4a81256a0e3c5e7e6d630a6e9928c7dcd33ed
to-wander <iswandering@gmail.com>
2023-02-25 00:21:29 +0800

    feat: vue2-test升级为2.7

---
+++ vue2-test/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="link" @click="changeName('hello')">{{ name }}</div>
+</template>
+
+<script setup lang="ts">
+interface propsType {
+  name: string;
+}
+
+const props = withDefaults(defineProps<propsType>(), {
+  name: "Hello World",
+});
+
+const changeName = () => {
+  // @ts-ignore
+  console.log(name);
+};
+</script>
+
+<style lang="scss">
+.link {
+  cursor: pointer;
+  width: 20px;
+  background-color: aquamarine;
+}
+</style>
